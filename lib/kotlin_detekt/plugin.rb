86a637d1e91b10f1732a6a72c97a4f54f0b8b19c
Pavel Strelchenko <p.strelchenko@hh.ru>
2021-10-08 13:24:22 +0300

    One more fix

--- lib/kotlin_detekt/plugin.rb
+++ lib/kotlin_detekt/plugin.rb
@@ -182,9 +182,9 @@ module Danger
 
     def get_message(issue, filename, line)
       file_path = """
-      ```
+
       #{filename}:#{line}
-      ```
+
       """
       open_link = "[Open in Android Studio](http://localhost:8091?message=#{filename}:#{line})"
       show_issue_source ? "#{issue.get("source")}: #{issue.get("message")} \n**Scroll to copy file name**\n#{file_path}\n\n#{open_link}" : issue.get("message")
